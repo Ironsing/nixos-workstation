63c9a5728c5796f9deac33101ffb1877836debb2
Ironsing <82096630+Ironsing@users.noreply.github.com>
2026-05-20 15:38:05 +0530

    first somewhat working version; at least hyprland is working fully

---
+++ dotfiles/.config/viegphunt/key_hints.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if pidof yad > /dev/null; then
+    pkill yad
+fi
+
+yad --center --title="Keybinding Hints" --no-buttons --list \
+    --column=Key: --column="" --column=Description: \
+    --timeout-indicator=bottom \
+"  =   "          "        "  "SUPER KEY (Windows Key Button)" \
+"" "" "" \
+"  H"              "        "  "Show keybinding hints" \
+"  Space"          "        "  "Open terminal" \
+"  E"              "        "  "Open file manager" \
+"  B"              "        "  "Open browser" \
+"" "" "" \
+"  Shift Ctrl Esc" "        "  "Exit Hyprland" \
+"  Q"              "        "  "Close active window" \
+"  Shift Q"        "        "  "Kill active window by PID" \
+"" "" "" \
+"  F"              "        "  "Toggle floating" \
+"  P"              "        "  "Toggle pseudo (dwindle)" \
+"  J"              "        "  "Toggle split (dwindle)" \
+"" "" "" \
+"  L"              "        "  "Lock screen" \
+"ALT Space"         "        "  "App launcher" \
+"  ."              "        "  "Emoji selector" \
+"  V"              "        "  "Clipboard manager" \
+"  W"              "        "  "Choose wallpaper" \
+"  Shift W"        "        "  "Random wallpaper" \
+"  Shift S"        "        "  "Screenshot (region)" \
+"" "" "" \
+"  [1 -> 0]"       "        "  "Switch workspace 1-10" \
+"  Shift [1 -> 0]" "        "  "Move window to workspace 1-10" \
+"" "" "" \
+"All bindings"   "        "  "$HOME/.config/hypr/conf/keybinding.conf"
